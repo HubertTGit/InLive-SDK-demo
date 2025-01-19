a45dde3d6d4c079e40a84fdd2c61c4c83dedac14
HuberttGit <hubert.talib@gmail.com>
2025-01-19 10:27:17 +0100

    prepare UI for multimedia

--- components/multimedia-app.tsx
+++ components/multimedia-app.tsx
@@ -1,12 +1,63 @@
 'use client';
+import { useMultimedia } from '@/context/multimedia.context';
 import { usePeer } from '@/context/peer.context';
+import { useEffect, useRef, useState } from 'react';
+import { Application } from 'pixi.js';
+import { pixijsStuff } from '@/lib/utils';
+import { Button } from './ui/button';
+import { Textarea } from './ui/textarea';
 
 export const Multimedia = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [media, setMedia] = useState<MediaStream | null>(null);
+  const [msg, setMsg] = useState<string>('');
   const { roomId } = usePeer();
+  const { messages, sendMessages, join, leave, peer } = useMultimedia();
+
+  useEffect(() => {
+    const app = new Application();
+
+    setTimeout(async () => {
+      await pixijsStuff(app, containerRef.current!);
+
+      const stream = app.canvas.captureStream();
+
+      setMedia(stream);
+    }, 100);
+  }, []);
 
   return (
     <>
-      <h1>Join: #{roomId}</h1>
+      <div className="flex gap-3">
+        <h1>Join: #{roomId}</h1> <Button>Join</Button>
+      </div>
+      <div>
+        <div
+          ref={containerRef}
+          style={{ height: '500px', width: '500px' }}
+        ></div>
+      </div>
+
+      <div>
+        <Textarea
+          placeholder="write anything then press ENTER"
+          value={msg}
+          onChange={(e) => setMsg(e.currentTarget.value)}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              sendMessages(msg);
+              setMsg('');
+            }
+          }}
+        />
+        <ul>
+          {messages.map((msg, index) => (
+            <li key={index} className=" bg-slate-400 p-2 mb-2 rounded-md">
+              {msg}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,39 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { Application, Graphics } from 'pixi.js';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const pixijsStuff = async (
+  app: Application,
+  element: HTMLDivElement
+) => {
+  await app.init({
+    background: '#1099bb',
+    resizeTo: element,
+  });
+
+  element.appendChild(app.canvas);
+
+  const circle = app.stage.addChild(
+    new Graphics()
+      .circle(0, 0, 8)
+      .fill({ color: 0xffffff })
+      .stroke({ color: 0x111111, alpha: 0.87, width: 1 })
+  );
+
+  circle.position.set(app.screen.width / 2, app.screen.height / 2);
+
+  // Enable interactivity!
+  app.stage.eventMode = 'static';
+
+  // Make sure the whole canvas area is interactive, not just the circle.
+  app.stage.hitArea = app.screen;
+
+  // Follow the pointer
+  app.stage.addEventListener('pointermove', (e) => {
+    circle.position.copyFrom(e.global);
+  });
+};
